dda90c3632905446de71478cab6c57b084715426
anagarcia791 <amgt2697@gmail.com>
2022-05-16 19:48:25 -0500

    correcion identacion

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // IMPORTACIONES DE REACT PARA INICIALIZACION Y ROUTER
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppRouter from './router/AppRouter.js';
+import AppRouter from './routers/AppRouter.js';
 import './styles/index.css'
 
 // ENLACE DE RUTAS PARA CONTENIDO DE DIV EN HTML
